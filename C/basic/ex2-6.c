803d482a0cade5cc530acec366703cad32392102
rainjin1 <106144161+rainjin1@users.noreply.github.com>
2026-02-04 14:51:35 +0900

    Create ex2-6.c

---
+++ C/basic/ex2-6.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+int main()
+{
+	printf("%.1lf\n", 1e6);
+	printf("%.7lf\n", 3.14e-5);
+	printf("%le\n", 0.000314);
+	printf("%.2le\n", 0.0000314);
+	
+
+	return 0;
+}
